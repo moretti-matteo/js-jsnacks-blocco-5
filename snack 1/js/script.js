043cfddb4f308f10b07501c6cb43bee28303dd61
moretti-matteo <matteomoretti2000@hotmail.it>
2022-05-16 12:17:55 +0200

    first commit

---
+++ snack 1/js/script.js
@@ -0,0 +1,9 @@
+// Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
+// Calcola quanto pesano tutte le zucchine.
+
+class Zucchina {
+    constructor(peso,lunghezza) {
+        this.peso = peso;
+        this.lunghezza = lunghezza;
+    }
+}
---
+++ snack 3/js/myFunc.js
@@ -0,0 +1,7 @@
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) - min;
+}
+
+function rangeNumCheck(number, min, max) {
+    return number >= min && number <= max ? true : false;
+}
