3614381306f6ee76b9dc175f952ea8622e4262de
moretti-matteo <matteomoretti2000@hotmail.it>
2022-05-16 12:22:51 +0200

    snack 1

--- snack 1/js/myFunc.js
+++ snack 1/js/myFunc.js
@@ -1,5 +1,5 @@
 function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) - min;
+    return Math.floor(Math.random() * (max - min + 1)- min) ;
 }
 
 function rangeNumCheck(number, min, max) {
--- snack 1/js/script.js
+++ snack 1/js/script.js
@@ -2,8 +2,23 @@
 // Calcola quanto pesano tutte le zucchine.
 
 class Zucchina {
-    constructor(peso,lunghezza) {
+    constructor(peso, lunghezza) {
         this.peso = peso;
         this.lunghezza = lunghezza;
     }
 }
+
+const zucchine = [];
+let tot = 0;
+
+for (let i = 0; i < 10; i++) {
+    let peso = randomNumber(1, 10);
+    let lunghezza = randomNumber(1, 20);
+    zucchine.push(new Zucchina(peso, lunghezza));
+    
+    tot+= zucchine[i].peso;
+}
+
+console.log(zucchine);
+console.log(tot);
+
